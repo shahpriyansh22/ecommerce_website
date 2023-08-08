9a22018fc774b1c51d1d121d42fc843eecaf44f8
Priyansh <hellopriyansh@gmail.com>
2023-08-08 16:39:18 +0545

    loginreg designed

--- java.js
+++ java.js
@@ -11,3 +11,16 @@ menu.onclick = () =>{
     menu.classList.toggle('bx-x');
     navmenu.classList.toggle('open')
 }
+
+// login and registration
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=>{
+    wrapper.classList.remove('active');
+});
